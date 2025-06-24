a842bf730ce12d5145874389c95616956797d638
verbovnik <118874696+verbovnik@users.noreply.github.com>
2025-06-24 09:27:09 +0200

    Update splash_screen.dart

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -1,37 +1,37 @@
-
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'nickname_entry_screen.dart';
+import 'chat_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _opacity;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-    _opacity = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
-    _controller.forward();
-
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacementNamed('/nickname_entry');
-    });
+    _checkNickname();
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _checkNickname() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? nickname = prefs.getString('nickname');
+
+    // Затримка для анімації splash-екрану
+    await Future.delayed(Duration(seconds: 2));
+
+    if (nickname == null || nickname.isEmpty) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => NicknameEntryScreen()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => ChatScreen(nickname: nickname)),
+      );
+    }
   }
 
   @override
@@ -39,13 +39,7 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: FadeTransition(
-          opacity: _opacity,
-          child: Image.asset(
-            'assets/images/ghost_logo.png',
-            width: 200,
-          ),
-        ),
+        child: Image.asset('assets/images/logo.png', width: 200),
       ),
     );
   }
