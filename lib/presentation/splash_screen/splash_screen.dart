ef67f36e003cdab6b3fa206759986e0e500237c3
verbovnik <118874696+verbovnik@users.noreply.github.com>
2025-06-24 03:25:53 +0200

    Add files via upload

---
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _opacity = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/nickname_entry');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: FadeTransition(
+          opacity: _opacity,
+          child: Image.asset(
+            'assets/images/ghost_logo.png',
+            width: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
